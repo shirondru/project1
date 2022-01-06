d2b85061f7a11a350b91be9df7ff6c9e77e16b5f
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-05 21:38:38 -0800

    added solution

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -6,10 +6,21 @@ def transcribe(seq: str) -> str:
     transcribes DNA to RNA by replacing
     all `T` to `U`
     """
+    assert "U" not in seq, "Input Sequence is not DNA"
 
+    seq = seq.upper() # Force all seqs to be capitalized
+    seq = seq.replace("T","U") # replace T with U
+    return seq
 
 def reverse_transcribe(seq: str) -> str:
     """
     transcribes DNA to RNA by replacing
     all `T` to `U` then reverses the sequence
     """
+    assert "U" not in seq, "Input Sequence is not DNA"
+
+	seq = seq.upper() # Force all seqs to be capitalized
+    seq = seq.replace("T","U") # replace T with U
+    seq = seq[::-1] #reverse seq
+    return seq
+
