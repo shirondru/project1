c8e25908677eed0e1bde9fb83729a503e3d82f4e
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-06 16:42:07 -0800

    add main function

--- main.py
+++ main.py
@@ -9,21 +9,46 @@ def main():
     The main function
     """
     # Create instance of FastaParser
-    # Create instance of FastqParser
-        
+    fasta_parser = FastaParser("data/test.fa")
+
+    # Create instance of FastaParser
+    fastq_parser = FastqParser("data/test.fq")
+
+
     # For each record of FastaParser, Transcribe the sequence
     # and print it to console
-       
-    # For each record of FastqParser, Transcribe the sequence
-    # and print it to console
-
-
     # For each record of FastaParser, Reverse Transcribe the sequence
     # and print it to console
-       
+    print('*'*20)
+    print('Parsing Fasta')
+    print('*'*20)
+    for record in fasta_parser:
+        header = record[0]
+        seq = record[1]
+        transcribed_seq = transcribe(seq)
+        reverse_transcrived_seq = reverse_transcribe(seq)
+        print(header)
+        print(f"Transcribed: {transcribed_seq}") 
+        print(f"Reverse Transcribed: {reverse_transcrived_seq}")
+        print('\n')
+
+
+    # For each record of FastqParser, Transcribe the sequence
+    # and print it to console
     # For each record of FastqParser, Reverse Transcribe the sequence
     # and print it to console
-
+    print('*'*20)
+    print('Parsing Fastq')
+    print('*'*20)
+    for record in fastq_parser:
+        header = record[0]
+        seq = record[1]
+        transcribed_seq = transcribe(seq)
+        reverse_transcrived_seq = reverse_transcribe(seq)
+        print(header)
+        print(f"Transcribed: {transcribed_seq}") 
+        print(f"Reverse Transcribed: {reverse_transcrived_seq}")
+        print('\n')
 
 """
 When executing a python script from the command line there will
