90fb52fecf8c7b3a101bd5c98fabb11193f2d1f9
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-08 10:45:47 -0800

    Reverse transcribe by calling transcribe func tehn reverse

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -3,25 +3,46 @@
 
 def transcribe(seq: str) -> str:
     """
-    transcribes DNA to RNA by replacing
-    all `T` to `U`
+    transcribes DNA to RNA by generating
+    the complement sequence with T -> U replacement
+
+
+    input  : A C T G A A C C C
+             | | | | | | | | |
+    output : U G A C U U G G G
     """
 
     assert "U" not in seq, "Input Sequence is not DNA" #Warn user they are attempting to transcribe RNA
+    
+    #map of nucleotides to complement, accounting for T->U in RNA. Replacing key with value will transcribe DNA -> RNA
+    transcription_map = {
+                    'C':'G',
+                    'G':'C',
+                    'T':'A',
+                    'A':'U'
+    }
 
-    seq = seq.upper() # Force all seqs to be capitalized. In case givev seq has weird casing
-    seq = seq.replace("T","U") # scans seq and replaces each T with a U
-    return seq
+    seq = seq.upper() # Force all seqs to be capitalized. In case given seq has weird casing
+    new_seq = "" # This function is transcribing seq by building a new sequence base-by-base
+    
+    for base in seq:
+        complement = transcription_map[base] #Get correct replacement nucleotide for the current nucleotide in the loop
+        new_seq += complement #Add complement of original DNA nucleotide base to same position in new_seq. This accounts for T->U because transcription_map accounts for T->U
+    
+
+    return new_seq
 
 def reverse_transcribe(seq: str) -> str:
     """
-    transcribes DNA to RNA by replacing
-    all `T` to `U` then reverses the sequence
-    """
-    assert "U" not in seq, "Input Sequence is not DNA" #Warn user they are attempting to transcribe RNA
+    transcribes DNA to RNA then reverses
+    the strand
+    input       : A C T G A A C C C
+                  | | | | | | | | |
+    transcribe  : U G A C U U G G G 
 
-    seq = seq.upper() # Force all seqs to be capitalized
-    seq = seq.replace("T","U")# scans seq and replaces each T with a U
-    seq = seq[::-1] #Slicing string with default start, default stop, and -1 step causes Python to start from the end and stop at the start, reversing the string
-    return seq
+    output : G G G U U C A G U
+    """
+    rna = transcribe(seq) #first transcribe the sequence using function already defined
+    reversed_rna = rna[::-1] #Reverse the seq. Slicing string with default start, default stop, and -1 step causes Python to start from the end and stop at the start, reversing the string
+    return reversed_rna
 
