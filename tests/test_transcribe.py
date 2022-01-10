a0cbcb7b550b8f1f355e4b045e5a3a3033bca88b
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-10 14:41:00 -0800

    More comments

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -27,6 +27,18 @@ def test_transcribe():
     Write your unit test for the
     transcribe function here.
     """
+    ##################################################################################################################
+    #Creating 5 tests including: 
+    #Two random DNA sequences to ensure the function can properly transcribe a string of ATGC nucleotides
+    #The exact DNA sequence given as an example by the TAs in the README file to ensure the function returns precisely what they expected
+    #An empty string to ensure the function doesn't break and properly returns an empty string
+    #The same nucleotide repeated over and over to ensure it doesn't break and properly returns its complement (also repeated)
+
+    #the unit test does not test for certain edge cases, such as if a non-DNA nucleotide was used in an input string.
+    #This is because the original transcribe function asserts that "U" is not in the input string to enfore DNA only
+    #And beacuse we were told we did not need to consder edge cases
+    ##################################################################################################################
+
 
     # if seq is an empty string
     test0={
@@ -52,7 +64,7 @@ def test_transcribe():
                 'expected_output':'UUUUUUU'
     }
 
-    #example from README
+    #example from README. The expected output was defined by the TAs
     test4 = {
                 'input': 'ACTGAACCC', 
                 'expected_output' : 'UGACUUGGG'
@@ -60,7 +72,7 @@ def test_transcribe():
 
     tests = [test0,test1,test2,test3,test4]
 
-    for test in tests:
+    for test in tests: #loop through each test and assert that if the input were passed into transcribe() that the expected output is returned
         assert transcribe(test['input']) == test['expected_output']
 
 
@@ -71,6 +83,11 @@ def test_reverse_transcribe():
     Write your unit test for the
     reverse transcribe function here.
     """
+
+    #####################################################################################################################################
+    #Using 5 tests with same inputs as in the transcribe function, but with the outputs reversed
+    
+    #####################################################################################################################################
 
     # if seq is an empty string
     test0={
@@ -97,17 +114,17 @@ def test_reverse_transcribe():
                 'expected_output':'UUUUUUU'
     }
 
-    #example from README
+    #example from README. The expected output was defined by the TAs
     test4 = {
                 'input': 'ACTGAACCC',
                 'expected_output' : 'GGGUUCAGU'
     }
-    #if seq 
+    
     
 
     tests = [test0,test1,test2,test3,test4]
 
-    for test in tests:
+    for test in tests: #loop through each test and assert that if the input were passed into reverse_transcribe() that the expected output is returned
         assert reverse_transcribe(test['input']) == test['expected_output']
 
 
